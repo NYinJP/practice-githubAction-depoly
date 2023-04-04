b0d3d7b6a52956e8cf6f8a37369507ed09560f20
sksDud <hinagogo@naver.com>
2023-04-04 13:30:57 +0900

    first upload

---
+++ src/main/java/com/codestates/practicegithubActiondepoly/PracticeGithubActionDepolyApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.practicegithubActiondepoly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeGithubActionDepolyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeGithubActionDepolyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/practicegithubActiondepoly/PracticeGithubActionDepolyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.practicegithubActiondepoly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGithubActionDepolyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
